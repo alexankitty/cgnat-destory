ad64351f1727fffddaba1744e88c4e416de1fa2a
Alexandra <alexankitty@gmail.com>
2024-04-11 18:31:11 -0600

    add ufw rule and more vars

--- serverforwarding.sh
+++ serverforwarding.sh
@@ -4,6 +4,9 @@ PORT_FILE="$SCRIPT_DIR/ports.txt"
 PORT_SCRIPT="$SCRIPT_DIR/pfneo.sh"
 TUN_IP="10.252.1.2"
 ROUTER_NET="192.168.1.0/24"
+TUN_IF="wg1"
+IF="eth0"
+ufw route allow in on $TUN_IF out on $IF
 ip route add $ROUTER_NET via $TUN_IP
 iptables -t nat -A POSTROUTING -s $TUN_IP -j MASQUERADE
 cat $PORT_FILE | while read line; do $PORT_SCRIPT $line; done
--- serverunforward.sh
+++ serverunforward.sh
@@ -4,6 +4,9 @@ PORT_FILE="$SCRIPT_DIR/ports.txt"
 PORT_SCRIPT="$SCRIPT_DIR/pfdown.sh"
 TUN_IP="10.252.1.2"
 ROUTER_NET="192.168.1.0/24"
+TUN_IF="wg1"
+IF="eth0"
+ufw route delete allow in on $TUN_IF out on $IF
 ip route del $ROUTER_NET
 iptables -t nat -D POSTROUTING -s $TUN_IP -j MASQUERADE
 cat $PORT_FILE | while read line; do $PORT_SCRIPT $line; done
